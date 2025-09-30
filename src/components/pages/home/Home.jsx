cf58b64bdd29c5fb54a5cda9e2cd7a5f84aaacb0
stivcode21 <stivenelcodificador@gmail.com>
2025-09-29 22:01:24 -0500

    feat: new page Home

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../../templates/mainLayout/MainLayout";
+
+const Home = () => {
+  return (
+    <MainLayout>
+      <div>Home</div>
+    </MainLayout>
+  );
+};
+
+export default Home;
