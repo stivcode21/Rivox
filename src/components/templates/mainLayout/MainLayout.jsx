65a8bf2d9e4106912b05f2a21bac34725ec592bd
stivcode21 <stivenelcodificador@gmail.com>
2025-10-01 21:24:38 -0500

    fix: imports in  mainlayout and their media queries

--- src/components/templates/mainLayout/MainLayout.jsx
+++ src/components/templates/mainLayout/MainLayout.jsx
@@ -1,21 +1,45 @@
-import { useCollapseSidebar } from "../../../store/collapseSidebar";
-import CollapseButton from "../../atoms/collapseButton/CollapseButton";
-import Sidebar from "../../organisms/sidebar/Sidebar";
-import UtilityBar from "../../organisms/utilityBar/UtilityBar";
+import { useCollapseSidebar } from "@/store/collapseSidebar";
+import CollapseButton from "@/components/atoms/collapseButton/CollapseButton";
+import Sidebar from "@/components/organisms/sidebar/Sidebar";
+import UtilityBar from "@/components/organisms/utilityBar/UtilityBar";
 import styles from "./MainLayout.module.css";
+import { useEffect } from "react";
+import Player from "@/components/organisms/player/Player";
 
 const MainLayout = ({ children }) => {
-  const { currentState } = useCollapseSidebar();
+  const { currentState, setCurrentState } = useCollapseSidebar();
+
+  //ocultar sidebar en pantallas pequeñas
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setCurrentState(false);
+      } else {
+        setCurrentState(true);
+      }
+    };
+
+    handleResize();
+
+    // Ejecutar al inicio
+    window.addEventListener("resize", handleResize);
+
+    // Limpiar el event listener al desmontar
+    return () => window.removeEventListener("resize", handleResize);
+  }, [setCurrentState]);
 
   return (
     <div className={styles.background}>
       <div className={styles.effect}>
         <section className={styles.container}>
-          {currentState && <Sidebar />}
-          <div className={styles.content}>
-            {children}
+          <article className={styles.sidebar}>
+            {currentState && <Sidebar />}
+          </article>
+          <article className={`${styles.content}`}>
+            <div className={styles.children}>{children}</div>
             {!currentState && <CollapseButton />}
-          </div>
+            <Player />
+          </article>
           <UtilityBar />
         </section>
       </div>
