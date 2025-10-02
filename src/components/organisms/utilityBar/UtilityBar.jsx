41aed7e8efa0d06d4d44df00c178b2e9079d6d30
stivcode21 <stivenelcodificador@gmail.com>
2025-10-01 21:27:20 -0500

    feat: costumized all the component utilityBar

--- src/components/organisms/utilityBar/UtilityBar.jsx
+++ src/components/organisms/utilityBar/UtilityBar.jsx
@@ -1,7 +1,43 @@
+import { Bell, CircleQuestionMark, Crown, Settings } from "lucide-react";
 import styles from "./UtilityBar.module.css";
+import logo from "../../../assets/mclovin.jpg";
 
 const UtilityBar = () => {
-  return <div className={styles.container}>UtilityBar</div>;
+  return (
+    <section className={styles.container}>
+      <article className={styles.pro}>
+        <Crown className={styles.icon} />
+        <h2 className={styles.title}>
+          <span>Go</span>
+          PRO
+        </h2>
+      </article>
+      <nav>
+        <ul className={styles.list}>
+          <button>
+            <li className={styles.item}>
+              <CircleQuestionMark />
+            </li>
+          </button>
+          <button>
+            <li className={styles.item}>
+              <Bell />
+            </li>
+          </button>
+          <button>
+            <li className={styles.item}>
+              <Settings />
+            </li>
+          </button>
+          <button>
+            <li className={styles.item}>
+              <img src={logo} className={styles.img} alt="imagen-de-perfil" />
+            </li>
+          </button>
+        </ul>
+      </nav>
+    </section>
+  );
 };
 
 export default UtilityBar;
