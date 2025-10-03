b710114f07026f1b0e6327f88e2243bd530fe0e9
stivcode21 <stivenelcodificador@gmail.com>
2025-10-02 22:48:44 -0500

    feat: created new structure for component sidebar and import all their componets

--- src/components/organisms/sidebar/Sidebar.jsx
+++ src/components/organisms/sidebar/Sidebar.jsx
@@ -1,10 +1,41 @@
+import { Ellipsis, Plus, PlusIcon } from "lucide-react";
 import styles from "./Sidebar.module.css";
+import SearchBar from "../../atoms/searchBar/SearchBar";
+import Navbar from "../../molecules/navbar/Navbar";
+import PlayLists from "../../molecules/playLists/PlayLists";
+import CollapseButton from "../../atoms/collapseButton/CollapseButton";
 
 const Sidebar = () => {
   return (
     <div className={styles.container}>
-      <img className={styles.img} src="./LogoRivox.svg" alt="Logo-Rivox" />
-      <div>sidebar</div>
+      <header className={styles.header}>
+        <img className={styles.img} src="./LogoRivox.svg" alt="Logo-Rivox" />
+        <button>
+          <Ellipsis className={styles.icon} />
+        </button>
+      </header>
+      <div className={styles.containerSearch}>
+        <SearchBar />
+      </div>
+      <section>
+        <article>
+          <h1 className={styles.title}>menu</h1>
+          <Navbar />
+        </article>
+      </section>
+
+      <section>
+        <article>
+          <div className={styles.playlistHeader}>
+            <h1 className={styles.title}>Playlist</h1>
+            <button>
+              <PlusIcon className={styles.icon} />
+            </button>
+          </div>
+          <PlayLists />
+        </article>
+      </section>
+      <CollapseButton />
     </div>
   );
 };
