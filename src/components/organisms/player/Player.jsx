a01107df355606dea2fc877f6574405d0f56bbde
stivcode21 <stivenelcodificador@gmail.com>
2025-10-02 22:46:08 -0500

    feat: costumized component Player

--- src/components/organisms/player/Player.jsx
+++ src/components/organisms/player/Player.jsx
@@ -1,7 +1,19 @@
+import ControlsPlayer from "../../atoms/controlsPlayer/ControlsPlayer";
 import styles from "./Player.module.css";
+import poster from "../../../assets/poster-mojandoAsientos.webp";
 
 const Player = () => {
-  return <div className={styles.container}>Player</div>;
+  return (
+    <section className={styles.container}>
+      <img src={poster} className={styles.poster} alt="poster de la cancion" />
+      <div className={styles.player}>
+        <div className={styles.controls}>
+          <ControlsPlayer />
+        </div>
+        <div className={styles.bar}></div>
+      </div>
+    </section>
+  );
 };
 
 export default Player;
