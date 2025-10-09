0fd78f9c8959f002d8f6f9b3dfd6c4e34ae475aa
stivcode21 <stivenelcodificador@gmail.com>
2025-10-08 20:48:26 -0500

    feat: customized all styles and layout of the section Home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,29 @@
-import MainLayout from "../../templates/mainLayout/MainLayout";
+import MusicFilters from "@/components/molecules/musicFilters/MusicFilters";
+import MainLayout from "@/components/templates/mainLayout/MainLayout";
 import styles from "./Home.module.css";
+import ListeSongs from "@/components/molecules/listeSongs/ListeSongs";
+import { BadgeCheck } from "lucide-react";
 
 const Home = () => {
   return (
     <MainLayout>
-      <section className={styles.content}>sad</section>
+      <div className={styles.banner}>
+        <div className={styles.img}></div>
+        <div className={styles.gradient}></div>
+
+        <span className={styles.number}>#1</span>
+        <div className={styles.containerArtist}>
+          <p className={styles.subtitle}>Artist</p>
+          <h2 className={styles.artista}>
+            Mike Towers
+            <span>
+              <BadgeCheck className={styles.icon} />
+            </span>
+          </h2>
+        </div>
+      </div>
+      <MusicFilters />
+      <ListeSongs />
     </MainLayout>
   );
 };
