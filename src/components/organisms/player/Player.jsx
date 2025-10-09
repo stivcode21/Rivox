f6a8deec99529e2a6a5bcccfccda52d92a2f39c4
stivcode21 <stivenelcodificador@gmail.com>
2025-10-08 20:52:39 -0500

    fix: solution the position in player for moment

--- src/components/organisms/player/Player.jsx
+++ src/components/organisms/player/Player.jsx
@@ -1,10 +1,13 @@
 import ControlsPlayer from "../../atoms/controlsPlayer/ControlsPlayer";
 import styles from "./Player.module.css";
 import poster from "../../../assets/poster-mojandoAsientos.webp";
+import { useCollapseSidebar } from "@/store/collapseSidebar";
 
 const Player = () => {
+  const { currentState } = useCollapseSidebar();
+
   return (
-    <section className={styles.container}>
+    <section className={`${styles.container} ${!currentState && styles.close}`}>
       <img src={poster} className={styles.poster} alt="poster de la cancion" />
       <div className={styles.player}>
         <div className={styles.controls}>
