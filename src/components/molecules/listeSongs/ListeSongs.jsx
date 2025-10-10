3928bbb7c6387e5aa7a7a2e1752b32f2e8069028
stivcode21 <stivenelcodificador@gmail.com>
2025-10-09 22:32:54 -0500

    feat: map all data for songs

--- src/components/molecules/listeSongs/ListeSongs.jsx
+++ src/components/molecules/listeSongs/ListeSongs.jsx
@@ -1,27 +1,26 @@
 import { Ellipsis, Heart } from "lucide-react";
 import styles from "./ListeSongs.module.css";
 import img from "@/assets/mike.webp";
+import { allSongs } from "../../../data/songs";
 
 const ListeSongs = () => {
   return (
     <nav className={styles.container}>
       <ul className={styles.list}>
-        <li className={styles.item}>
-          {
-            <>
-              <div className={styles.description}>
-                <span>01</span>
-                <img src={img} alt="poster-song" className={styles.img} />
-                <p>Mike</p>
-              </div>
-              <p>dasdsa</p>
-              <div>
-                <Heart className={styles.icon} />
-                <Ellipsis className={styles.icon} />
-              </div>
-            </>
-          }
-        </li>
+        {allSongs.map((song) => (
+          <li className={styles.item}>
+            <div className={styles.description}>
+              <span>{song.id + 1}</span>
+              <img src={song.img} alt="poster-song" className={styles.img} />
+              <p>{song.title}</p>
+            </div>
+            <p>{song.artist}</p>
+            <div>
+              <Heart className={styles.icon} />
+              <Ellipsis className={styles.icon} />
+            </div>
+          </li>
+        ))}
       </ul>
     </nav>
   );
