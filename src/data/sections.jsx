ce45f2ac31d715b039ababd9055ffe8b02b2f184
stivcode21 <stivenelcodificador@gmail.com>
2025-10-14 22:01:14 -0500

    feat: new data for each section

---
+++ src/data/sections.jsx
@@ -0,0 +1,8 @@
+import { Disc3, Heart, Home, UsersRound } from "lucide-react";
+
+export const Sections = [
+  { name: "Home", icon: <Home />, path: "" },
+  { name: "Favoritos", icon: <Heart />, path: "favorites" },
+  { name: "Albumes", icon: <Disc3 />, path: "albumes" },
+  { name: "Artistas", icon: <UsersRound />, path: "artistas" },
+];
