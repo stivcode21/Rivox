dd231f027a1452070bad6d41401245fd06ff5177
stivcode21 <stivenelcodificador@gmail.com>
2025-10-14 21:57:16 -0500

    feat: new page Artists section

---
+++ src/components/pages/artistas/Artistas.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "@/components/templates/mainLayout/MainLayout";
+
+const Artistas = () => {
+  return (
+    <MainLayout>
+      <div>artistas</div>
+    </MainLayout>
+  );
+};
+
+export default Artistas;
