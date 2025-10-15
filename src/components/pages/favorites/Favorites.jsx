91703ab8b040074896777818655f24664dfe3e8e
stivcode21 <stivenelcodificador@gmail.com>
2025-10-14 21:56:48 -0500

    feat: new page Favorites section

---
+++ src/components/pages/favorites/Favorites.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "@/components/templates/mainLayout/MainLayout";
+
+const Favorites = () => {
+  return (
+    <MainLayout>
+      <div>favorites</div>
+    </MainLayout>
+  );
+};
+
+export default Favorites;
