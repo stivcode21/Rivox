83bf8999f8249e87f36984900b8e97d1eea405f0
stivcode21 <stivenelcodificador@gmail.com>
2025-10-14 21:59:42 -0500

    feat: new page section Albumes

---
+++ src/components/pages/albumes/Albumes.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "@/components/templates/mainLayout/MainLayout";
+
+const Albumes = () => {
+  return (
+    <MainLayout>
+      <div>Albumes</div>
+    </MainLayout>
+  );
+};
+
+export default Albumes;
