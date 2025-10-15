816b2b80f78f1a5da502063f2fc179fe02e44099
stivcode21 <stivenelcodificador@gmail.com>
2025-10-14 21:58:42 -0500

    feat: added new pages each section

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "../components/pages/home/Home";
+import Favorites from "../components/pages/favorites/Favorites";
+import Albumes from "../components/pages/albumes/Albumes";
+import Artistas from "../components/pages/artistas/Artistas";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/albumes" element={<Albumes />} />
+        <Route path="/artistas" element={<Artistas />} />
       </Routes>
     </BrowserRouter>
   );
